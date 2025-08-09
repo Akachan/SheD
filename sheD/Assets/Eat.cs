74ba93b2b22efa818abf75b74aae31a50c611733
Akachan00 <chaves.mayra@gmail.com>
2025-08-09 01:32:04 -0300

    Update: Camuflaje básico

--- sheD/Assets/Eat.cs
+++ sheD/Assets/Eat.cs
@@ -6,8 +6,49 @@ using UnityEngine;
 
 public class Eat : MonoBehaviour
 {
+    
+    
+    
     [SerializeField] private TextMeshProUGUI foodValueText;
-    private int totalFood = 0;
+    private int _totalFood = 0;
+    private bool _isCamouflaged = false;
+    private float _currentTime = 0;
+    private GameObject _camouflajeObject;
+    public bool IsCamuflaged => _isCamouflaged;
+
+
+    private void Update()
+    {
+        UpdateText();
+        Camouflage();
+        
+    }
+
+    private void Camouflage()
+    {
+        if (_isCamouflaged)
+        {
+            _currentTime += Time.deltaTime;
+            if (_currentTime >= 1)
+            {
+                RemoveFood();
+                _currentTime = 0;
+            }
+        }
+    }
+
+    private void RemoveFood()
+    {
+        _totalFood--;
+
+        if (_totalFood == 0)
+        {
+            _isCamouflaged = false;
+            _camouflajeObject.SetActive(false);
+            _camouflajeObject = null;
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Food"))
@@ -15,19 +56,43 @@ public class Eat : MonoBehaviour
             print("comida");
             other.gameObject.SetActive(false);
             AddFood();
-            UpdateText();
         }
+
+        if (other.gameObject.CompareTag("Camuflaje"))
+        {
+
+            print("camuflaje On");
+            _camouflajeObject = other.gameObject;
+            _isCamouflaged = true;
+            AddFood();
+            
+
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        
+        if (other.gameObject.CompareTag("Camuflaje"))
+        {
+            print("camuflaje Off");
+            _isCamouflaged = false;
+            other.gameObject.SetActive(false);
+            _camouflajeObject = null;
+        }
+        
+        
     }
 
 
     private void AddFood()
     {
-        totalFood++;
+        _totalFood++;
         
     }
 
     private void UpdateText()
     {
-        foodValueText.text = totalFood.ToString();
+        foodValueText.text = _totalFood.ToString();
     }
 }
--- sheD/Assets/Player.cs
+++ sheD/Assets/Player.cs
@@ -9,16 +9,19 @@ public class Player : MonoBehaviour
    [SerializeField] private float moveSpeed = 5f;
    private SpriteRenderer _spriteRenderer;
    [SerializeField] GameObject foodArea;
+   private Eat _eat;
   
 
    private void Awake()
    {
       _input = FindFirstObjectByType<InputManager>();
       _spriteRenderer = GetComponent<SpriteRenderer>();
+      _eat = FindFirstObjectByType<Eat>();
    }
 
    private void FixedUpdate()
    {
+      Camuflaje();
       MovePlayer();
    }
 
@@ -34,6 +37,22 @@ public class Player : MonoBehaviour
          scale.x = _input.MovementValue.x < 0 ? -1 : 1;
          foodArea.transform.localScale = scale;
          
+      }
+   }
+
+   private void Camuflaje()
+   {
+      if (_eat.IsCamuflaged)
+      {
+         var color = _spriteRenderer.color;
+         color.a = 0.5f;
+         _spriteRenderer.color = color;
+      }
+      else
+      {
+         var color = _spriteRenderer.color;
+         color.a = 1f;
+         _spriteRenderer.color = color;
       }
    }
 }
