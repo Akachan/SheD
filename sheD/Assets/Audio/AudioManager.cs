456f00bfeb544edaa2ec0cfdc2a437da498819b9
Driannn <drianbetancourt@gmail.com>
2025-08-10 18:00:24 -0300

    musica cambia al transformarse (sonido de caminar ya esta). al cambiar de scena se rompe xd

--- sheD/Assets/Audio/AudioManager.cs
+++ sheD/Assets/Audio/AudioManager.cs
@@ -1,11 +1,43 @@
 using UnityEngine;
 using FMODUnity; // Asegúrate de tener la integración de FMOD en tu proyecto
+using FMOD.Studio;
+using static UnityEngine.Rendering.DebugUI;
 
 public class AudioManager : MonoBehaviour
 {
     // --- Singleton Pattern ---
     // La instancia estática pública que será accesible desde cualquier otro script.
     public static AudioManager Instance { get; private set; }
+
+    [SerializeField] private EventReference mainMusicRef;
+    private EventInstance _musicEventInstance;
+
+    private Player _player;
+
+
+
+
+
+    // Nombre exacto del parámetro global en FMOD Studio
+    public string globalParameterName = "GameState";
+
+    // Valor que quieres asignar
+    public float newValue = 0f;
+
+    void Start()
+    {
+        SetGlobalParameter(newValue);
+    }
+
+    public void SetGlobalParameter(float value)
+    {
+        FMOD.Studio.System system = RuntimeManager.StudioSystem;
+        // Cambia el valor del parámetro global
+        system.setParameterByName(globalParameterName, value);
+    }
+
+
+
 
     private void Awake()
     {
@@ -20,7 +52,44 @@ public class AudioManager : MonoBehaviour
             Destroy(gameObject);
             Debug.Log("AudioManager: Se encontró otra instancia, esta ha sido destruida.");
         }
+
+
+        _player = FindObjectOfType<Player>();
+
     }
+
+    private void Update()
+    {
+        CheckSnakeState();
+    }
+
+
+    void CheckSnakeState()
+    {
+        int state = 0;
+        if (_player.IsCamuflaged)
+        {
+            state = 1;
+        }
+        else
+        {
+            state = 0;
+        }
+
+        FMOD.Studio.System system = RuntimeManager.StudioSystem;
+        // Cambia el valor del parámetro global
+        system.setParameterByName("GlobalSnakeState", state);
+
+        //_musicEventInstance.setParameterByName("SnakeState", state);
+    }
+
+
+
+
+
+
+
+
 
     // --- Lógica de FMOD ---
     // Un método de ejemplo para reproducir un sonido 2D (One-Shot).
