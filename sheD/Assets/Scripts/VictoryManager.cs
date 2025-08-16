bc30f498a726bb60a8bf1ebae96ff5c56764ec28
Akachan00 <chaves.mayra@gmail.com>
2025-08-16 10:30:01 -0300

    Updated: SetCloseDoorSprite, adjust door position & fill direction. WinCondition set in Door

--- sheD/Assets/Scripts/VictoryManager.cs
+++ sheD/Assets/Scripts/VictoryManager.cs
@@ -1,54 +1,78 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Food;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class VictoryManager : MonoBehaviour
 {
+   [Header("Victory Condition")]
+   [SerializeField] private CommonFoodTypeName foodType;
+   [SerializeField] private int foodCountToWin;
+   
+   [Header("No taocar")]
    [SerializeField] private GameObject door;
+
+   [SerializeField] private GameObject closeDoor;
+   [SerializeField] private GameObject openDoor;
+   
    private BoxCollider2D _collider;
    private SpriteRevealController _sprite;
+
+   private bool _isOpen;
 
 
    private void Awake()
    {
-      _collider =door.GetComponent<BoxCollider2D>();
+      _collider = GetComponent<BoxCollider2D>();
       _sprite = door.GetComponent<SpriteRevealController>();
    }
 
    private void Start()
    {
-      SetColliderState(false);
-      SetDoorState(0f);
+      SetDoorState(LevelManager.Instance.FoodInventory.GetFoodRatio(foodType));
+   }
+
+   
+   private void Update()
+   {
+      SetDoorShader();
+
+      _isOpen = LevelManager.Instance.FoodInventory.GetFoodCount(foodType) >= foodCountToWin;
+   }
+
+   private void OnTriggerEnter2D(Collider2D other)
+   {
+      if (!other.CompareTag("Player")) return;
+      
+      Debug.Log("Player tocó puerta");
+      if (_isOpen)
+      {
+         StartCoroutine(LevelManager.Instance.EndGame(EndGameType.Win));
+      }
+      else
+      {
+         Debug.Log("No tienes suficiente material");
+         //poner algun mensaje de que le falta cierto material
+      }
+   }
+
+   private void SetDoorShader()
+   {
+      var foodCount = LevelManager.Instance.FoodInventory.GetFoodCount(foodType);
+      var fillValue = Mathf.Clamp(foodCount / (float) foodCountToWin,0f,1f);
+      SetDoorState(fillValue);
+      if (foodCount >= foodCountToWin)
+      {
+         closeDoor.SetActive(false);
+         openDoor.SetActive(true);
+      }
    }
 
    private void SetDoorState(float f)
    {
       _sprite.SetFillAmount(f);
    }
-
-   private void SetColliderState(bool state)
-   {
-      _collider.enabled = state;
-   }
-
-   private void Update()
-   {
-      var fillValue = LevelManager.Instance.CurrentFoodCount / (float)LevelManager.Instance.FoodValueToWin;
-     
-      SetDoorState(fillValue);
-      
-      if (fillValue >= 1)
-      {
-         SetColliderState(true);
-         
-      }
-      else
-      {
-         SetColliderState(false);
-      }
-   }
-
 
 }
