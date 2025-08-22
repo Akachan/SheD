5f14731e7b26c4199131be412e1b91a13d472904
Akachan00 <chaves.mayra@gmail.com>
2025-08-22 16:38:56 -0300

    Added: ´Pause, proximity & game over bgm in AudioManager

---
+++ sheD/Assets/EnemySfx.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+public class EnemySfx : MonoBehaviour
+{
+    [SerializeField] private EventReference footstepSfx;
+    private Player.Player _player;
+    public void StepSfx()
+    {
+        RuntimeManager.PlayOneShotAttached(footstepSfx,gameObject );
+        //print("STEP");
+    }
+
+    private void Awake()
+    {
+        _player = FindFirstObjectByType<Player.Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
